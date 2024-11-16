5c6e6ee3d72b0a1b8be9f58580c2456c1fdcabca
balabhargav21 <140328274+balabhargav21@users.noreply.github.com>
2024-11-16 11:02:29 +0530

    third commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState('')
+  const onhandleClick = (value) => {
+    setInput((prevalue) => prevalue + value);
+  }
+  const onEvaluate = () => {
+    try {
+      setInput(eval(input).toString());
+    }
+    catch (error) {
+      setInput('Syntax Error');
+    }
+  };
+  
+  const onClear = () => {
+    setInput('');
+  }
+  const onDelete = () => {
+    setInput( (prevalue) => prevalue.slice(0,-1));
+  }
+
+  return (
+    <>
+     <div className="calci">
+     <div className="calculator-board">
+        <div className="text-box">
+          <input type="text" value={input} readOnly />
+        </div>
+        <div className="button-row">
+          <button onClick={onClear}>C</button>
+          <button onClick={onDelete}>D</button>
+          <button onClick={() => {onhandleClick('/')}}>/</button>
+          <button onClick={() => {onhandleClick('%')}}>%</button>
+        </div>
+        <div className="button-row">
+          <button onClick={() => {onhandleClick('7')}}>7</button>
+          <button onClick={() => {onhandleClick('8')}}>8</button>
+          <button onClick={() => {onhandleClick('9')}}>9</button>
+          <button onClick={() => {onhandleClick('+')}}>+</button>
+        </div>
+        <div className="button-row">
+          <button onClick={() => {onhandleClick('4')}}>4</button>
+          <button onClick={() => {onhandleClick('5')}}>5</button>
+          <button onClick={() => {onhandleClick('6')}}>6</button>
+          <button onClick={() => {onhandleClick('-')}}>-</button>
+        </div>
+        <div className="button-row">
+          <button onClick={() => {onhandleClick('1')}}>1</button>
+          <button onClick={() => {onhandleClick('2')}}>2</button>
+          <button onClick={() => {onhandleClick('3')}}>3</button>
+          <button onClick={() => {onhandleClick('*')}}>*</button>
+        </div>
+        <div className="button-row">
+          <button onClick={() => {onhandleClick('0')}}>0</button>
+          <button onClick={() => {onhandleClick('.')}}>.</button>
+          <button className='equals' onClick={onEvaluate}>=</button>
+        </div>
+      </div>
+     </div>
+     
+    </>
+  )
+}
+
+export default App
